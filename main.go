f13aaf8c2aa4d26ef19779b7cc4634e9913eb710
Thiago Augustus <thiagoaugustusdeoliveira@gmail.com>
2015-10-30 12:29:22 +0000

    Debug

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+  "fmt"
   "flag"
   log "github.com/Sirupsen/logrus"
   "github.com/thiagoaugustus/GoLang-Structure/shared/envconfig"
 )
 
 var (
-  appenv = flag.String("config", "app.yaml", "")
-  debug	= flag.Bool("debug", false, "")
+  appenv = flag.String("config", "ap.yaml", "")
+  debug	= flag.Bool("debug", true, "")
 )
 
 func main() {
@@ -20,9 +21,15 @@ func main() {
   }
   
   // Load the configuration from yaml file
-  env := envconfig.Load(*appenv)
+  cfg, err := envconfig.Load(*appenv)
   
-  if env == nil {
-    
+  if err != nil {
+    log.WithFields(log.Fields{
+      "event": "Faild to get",
+      "topic": "Config",
+      "key"  : 24,
+    }).Fatal("Faild to get the configuration")    
   }
+  
+  fmt.Println(cfg.Dev.Server)
 }
