dbfed9a6fe7409834d1ca006341bdd4532a466f9
Thiago Augustus <thiagoaugustusdeoliveira@gmail.com>
2015-10-30 13:10:55 +0000

    Update Envconfig

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "fmt"
   "flag"
   log "github.com/Sirupsen/logrus"
   "github.com/thiagoaugustus/GoLang-Structure/shared/envconfig"
 )
 
 var (
-  appenv = flag.String("config", "ap.yaml", "")
-  debug	= flag.Bool("debug", true, "")
+  appenv = flag.String("config", "app.yaml", "")
+  envmod	= flag.String("envmod", "dev", "")
+  debug	= flag.Bool("debug", false, "")
 )
 
 func main() {
@@ -21,15 +21,15 @@ func main() {
   }
   
   // Load the configuration from yaml file
-  cfg, err := envconfig.Load(*appenv)
+  env, err := envconfig.Load(*appenv)
   
   if err != nil {
     log.WithFields(log.Fields{
       "event": "Faild to get",
       "topic": "Config",
-      "key"  : 24,
+      "code" : 24,
     }).Fatal("Faild to get the configuration")    
   }
   
-  fmt.Println(cfg.Dev.Server)
+  //store_ := datastore.Load(env.EnvMode(*envmod))
 }
