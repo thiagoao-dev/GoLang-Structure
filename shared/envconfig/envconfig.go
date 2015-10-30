8015f124a6f61c4a205128adc522bb5c939d5f43
Thiago Augustus <thiagoaugustusdeoliveira@gmail.com>
2015-10-30 11:15:49 +0000

    Fixed EnvConfig file var name misspelled

--- shared/envconfig/envconfig.go
+++ shared/envconfig/envconfig.go
@@ -42,9 +42,9 @@ type Env struct {
 // Load reads the environment file and reads variables in "key:value" yaml format.
 // Then it read the system environment variables. It returns the combined
 // results in a key value map.
-func Load(filepath string) Env, err {
+func Load(filepath string) (Env, err) {
   
-  cfgFile, err := ioutil.ReadFile(path)
+  cfgFile, err := ioutil.ReadFile(filepath)
   
   if err != nil {
     log.WithFields(log.Fields{
